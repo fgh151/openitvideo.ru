e06c42e62eff435cd484a82476aea2cb6a8d8c72
Fedor B Gorsky <fedor@support-pc.org>
2020-08-04 08:50:14 +0300

    delete unneeded dependencies in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,20 +4,20 @@ module.exports = {
         description: `AVideo collections`,
         author: `Fedor B Gorsky`,
     },
-  /* Your site config here */
-  plugins: [
+    /* Your site config here */
+    plugins: [
 
 
-      {
-          resolve: `gatsby-plugin-paginate`,
-          options: {
-              sources: [
-                  {
-                      path: `/page`,
-                      pageSize: 12,
-                      template: `${__dirname}/src/templates/index.js`,
-                      serialize: (results) => results.allVideosJson.edges,
-                      query: `{
+        {
+            resolve: `gatsby-plugin-paginate`,
+            options: {
+                sources: [
+                    {
+                        path: `/page`,
+                        pageSize: 12,
+                        template: `${__dirname}/src/templates/index.js`,
+                        serialize: (results) => results.allVideosJson.edges,
+                        query: `{
          allVideosJson {
     edges {
       node {
@@ -29,51 +29,43 @@ module.exports = {
     }
   }
     }`
-                  }
-              ]
-          }
-      },
+                    }
+                ]
+            }
+        },
 
 
-
-      {
-          resolve: `gatsby-plugin-manifest`,
-          options: {
-              name: `GatsbyJS translated tutorial`,
-              short_name: `GatsbyJS tutorial`,
-              start_url: `/`,
-              background_color: `#f7f0eb`,
-              theme_color: `#a2466c`,
-              display: `standalone`,
-              icon: `static/favicon.ico`,
-              include_favicon: true
-          }
-      },
-      'gatsby-plugin-offline',
-      'gatsby-transformer-json',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
-        ignore: [`**/\.*`], // ignore files starting with a dot
-      },
-    },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`
-      }
-    },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-styled-components',
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `GatsbyJS translated tutorial`,
+                short_name: `GatsbyJS tutorial`,
+                start_url: `/`,
+                background_color: `#f7f0eb`,
+                theme_color: `#a2466c`,
+                display: `standalone`,
+                icon: `static/favicon.ico`,
+                include_favicon: true
+            }
+        },
+        'gatsby-plugin-offline',
+        'gatsby-transformer-json',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `data`,
+                path: `${__dirname}/src/data/`,
+                ignore: [`**/\.*`], // ignore files starting with a dot
+            },
+        },
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `src/utils/typography`
+            }
+        },
+        'gatsby-plugin-sass',
+        'gatsby-plugin-styled-components',
         'gatsby-plugin-react-helmet',
-  ],
+    ],
 }
