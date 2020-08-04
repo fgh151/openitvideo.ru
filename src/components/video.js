e2bfff33cb529aa68b6198eab8f8448447524832
Fedor B Gorsky <fedor@support-pc.org>
2020-08-04 08:42:55 +0300

    Responsive video

--- src/components/video.js
+++ src/components/video.js
@@ -14,20 +14,22 @@ const Video = (props) => {
                 <img src={'https://img.youtube.com/vi/' + props.video.node.id + '/mqdefault.jpg'}
                      alt={props.video.node.title}/>
                 <p>
-                        {props.video.node.title}
+                    {props.video.node.title}
                 </p>
             </div>
-            <Modal show={show} onHide={handleClose}>
+            <Modal size="lg" show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>{props.video.node.title}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <iframe width="560" height="315"
-                            title={props.video.node.title}
-                            src={'https://www.youtube.com/embed/' + props.video.node.id}
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen></iframe>
+                    <div className="embed-responsive embed-responsive-16by9">
+                        <iframe className="embed-responsive-item"
+                                title={props.video.node.title}
+                                src={'https://www.youtube.com/embed/' + props.video.node.id}
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen></iframe>
+                    </div>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
