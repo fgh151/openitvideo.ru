59c6f9f061daa8dfd39ecf39fa1f982d6c27259a
Fedor B Gorsky <fedor@support-pc.org>
2020-09-08 13:22:38 +0300

    manifest + metrica

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,23 @@ module.exports = {
     },
     /* Your site config here */
     plugins: [
-
+        {
+            resolve: `gatsby-plugin-yandex-metrika`,
+            options: {
+                // The ID of yandex metrika.
+                trackingId: 67134169,
+                // Enabled a webvisor. The default value is `false`.
+                webvisor: true,
+                // Enables tracking a hash in URL. The default value is `false`.
+                trackHash: true,
+                // Defines where to place the tracking script - `false` means before body (slower loading, more hits)
+                // and `true` means after the body (faster loading, less hits). The default value is `false`.
+                afterBody: true,
+                // Use `defer` attribute of metrika script. If set to `false` - script will be loaded with `async` attribute.
+                // Async enables earlier loading of the metrika but it can negatively affect page loading speed. The default value is `false`.
+                defer: false,
+            },
+        },
 
         {
             resolve: `gatsby-plugin-paginate`,
@@ -38,11 +54,11 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `GatsbyJS translated tutorial`,
-                short_name: `GatsbyJS tutorial`,
+                name: `OpenItVideo`,
+                short_name: `OpenItVideo`,
                 start_url: `/`,
                 background_color: `#f7f0eb`,
-                theme_color: `#a2466c`,
+                theme_color: `#FF0000`,
                 display: `standalone`,
                 icon: `static/favicon.ico`,
                 include_favicon: true
